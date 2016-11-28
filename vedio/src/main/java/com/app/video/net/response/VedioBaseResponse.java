fed663aa081bb2b15c90559e4a4ad55b0a96fb3c
weijiangmin <weijiangmin@didichuxing.com>
2016-11-28 21:42:48 +0800

    video detail  model

---
+++ vedio/src/main/java/com/app/video/net/response/VedioBaseResponse.java
@@ -0,0 +1,17 @@
+package com.app.video.net.response;
+
+public class VedioBaseResponse<T, S> {
+
+    /**
+     * 返回数据对象
+     */
+    public T page;
+
+    /**
+     * 返回数据对象
+     */
+    public S list1;
+
+    public boolean success;
+
+}
---
+++ vedio/src/main/java/com/app/video/net/response/VideoDetailResponse.java
@@ -0,0 +1,11 @@
+package com.app.video.net.response;
+
+
+import com.app.basevideo.net.BaseHttpResult;
+import com.app.video.data.VideoDetailData;
+
+import java.util.List;
+
+public class VideoDetailResponse extends BaseHttpResult<List<VideoDetailData.Detail>> {
+
+}
