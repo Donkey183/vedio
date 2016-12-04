76c6047f5e941c722a4c4cf7d47eb78910550d11
liubohua <LIUBOHUA@KINGSOFT.COM>
2016-12-04 20:57:07 +0800

    aa

--- vedio/src/main/java/com/app/video/config/Config.java
+++ vedio/src/main/java/com/app/video/config/Config.java
@@ -13,8 +13,17 @@ public class Config {
     private Payoff pay1;
     private Payoff pay2;
     private @DrawableRes int pay_img;
+    private int playtime;
 
-    public Config(String vip_now, String tittle_first, int img_first1, int img_first2, String tittle_vip, int img_vip1, int img_vip2, Payoff pay1, Payoff pay2, int pay_img) {
+    public int getPlaytime() {
+        return playtime;
+    }
+
+    public void setPlaytime(int playtime) {
+        this.playtime = playtime;
+    }
+
+    public Config(String vip_now, String tittle_first, int img_first1, int img_first2, String tittle_vip, int img_vip1, int img_vip2, Payoff pay1, Payoff pay2, int pay_img,int playtime) {
         this.vip_now = vip_now;
         this.tittle_first = tittle_first;
         this.img_first1 = img_first1;
@@ -25,6 +34,7 @@ public class Config {
         this.pay1 = pay1;
         this.pay2 = pay2;
         this.pay_img = pay_img;
+        this.playtime = playtime;
     }
 
     public String getVip_now() {
